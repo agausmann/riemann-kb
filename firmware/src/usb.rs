214b76225b2c266201f5a544b08dfd8d3a98e554
Adam Gausmann <adam@gaussian.dev>
2022-11-01 18:22:24 -0500

    Avoid using MaybeUninit, use Option instead.

--- firmware/src/usb.rs
+++ firmware/src/usb.rs
@@ -1,4 +1,4 @@
-use core::{cell::Cell, mem::MaybeUninit};
+use core::cell::Cell;
 use cortex_m::{
     interrupt::{CriticalSection, Mutex},
     peripheral::NVIC,
@@ -54,7 +54,7 @@ pub struct Leds {
 }
 
 // Resources sent to the USB interrupt contexts.
-static mut USB_CTX: MaybeUninit<UsbContext> = MaybeUninit::uninit();
+static mut USB_CTX: Option<UsbContext> = None;
 
 // TODO more granular mutex based on which interrupts access this
 static USB_FLAGS: Mutex<UsbFlags> = Mutex::new(UsbFlags {
@@ -68,9 +68,9 @@ pub unsafe fn init(
     resets: &mut RESETS,
 ) {
     let usb_bus = {
-        static mut USB_BUS: MaybeUninit<UsbBusAllocator<UsbBus>> = MaybeUninit::uninit();
+        static mut USB_BUS: Option<UsbBusAllocator<UsbBus>> = None;
         unsafe {
-            USB_BUS.write(UsbBusAllocator::new(UsbBus::new(
+            USB_BUS.insert(UsbBusAllocator::new(UsbBus::new(
                 regs, dpram, clocks, true, resets,
             )))
         }
@@ -83,7 +83,7 @@ pub unsafe fn init(
         .device_release(0x0001)
         .build();
     unsafe {
-        USB_CTX.write(UsbContext {
+        USB_CTX = Some(UsbContext {
             device: usb_device,
             hid: usb_hid,
         });
@@ -93,18 +93,17 @@ pub unsafe fn init(
 
 pub unsafe fn state(cs: &CriticalSection) -> UsbDeviceState {
     let _ = cs;
-    unsafe { USB_CTX.assume_init_mut() }.device.state()
+    unsafe { USB_CTX.as_mut() }.unwrap().device.state()
 }
 
 pub unsafe fn borrow(cs: &CriticalSection) -> (&mut UsbContext, &UsbFlags) {
-    (unsafe { USB_CTX.assume_init_mut() }, USB_FLAGS.borrow(cs))
+    (unsafe { USB_CTX.as_mut() }.unwrap(), USB_FLAGS.borrow(cs))
 }
 
 #[interrupt]
 fn USBCTRL_IRQ() {
     cortex_m::interrupt::free(|cs| {
-        let ctx = unsafe { USB_CTX.assume_init_mut() };
-        let flags = USB_FLAGS.borrow(cs);
+        let (ctx, flags) = unsafe { borrow(cs) };
         ctx.poll(flags);
     })
 }
